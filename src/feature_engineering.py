64715a0fc1f7e0d619df9864dfa52cdaff5b7681
castares <cesar.castares@gmail.com>
2019-12-15 20:31:27 +0100

    commit

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -19,6 +19,17 @@ def getTokenlist(lst):
         print(f"movie: {movieid} processed")
     print(len(set(tokens_list)))
 
+def filterbyRatingsAmount(ratings_ddf, min_rt, max_rt):
+    #Receives a Dask Dataframe, a minimum and a maximum number of ratings per user.
+    #Returns a Dask Dataframe adding a column with ratings per user to every entrance, and filtering the users outside the given range of ratings. 
+    g_userId = ratings_ddf.groupby('userId')
+    user_ratings = g_userId['movieId'].count().compute()
+    user_ratings = user_ratings.reset_index()
+    ratings_ddf = ratings_ddf.merge(user_ratings, left_on='userId', right_on='userId').compute()
+    ratings_ddf = ratings_ddf.loc[ratings_ddf['movieId_y'] > min_rt]
+    ratings_ddf = ratings_ddf.loc[ratings_ddf['movieId_y'] < max_rt]
+    return ratings
+
 def main():
     #ratings = pd.read_csv("../input/ratings_small.csv")
     #metadata['keywords'] = metadata['overview'].apply(lambda x: processOverview(str(x)))
